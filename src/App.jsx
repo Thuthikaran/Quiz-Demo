62d00de22d32a0ed5a1982a6e0a5eba34c4ca202
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-03-13 22:37:06 +0100

    Changed the title of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-xl italic">
-        Ce quiz est conçu pour fournir des recommandations de bijoux
-        personnalisées en fonction de vos réponses.{' '}
-      </h1>
-      <h3 className=""> - Eloïne DA SILVA </h3>
+      <div className="py-3">
+        <h1 className="text-sm italic ">
+          Ce quiz est conçu pour fournir des recommandations de bijoux
+          personnalisées en fonction de vos réponses.{' '}
+        </h1>
+        <p className="text-center text-xs"> - Eloïne DA SILVA </p>
+      </div>
     </div>
   );
 }

6336eb3bfaeac7a9f1199a3fbe48411dc920fad8
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2025-03-13 21:11:40 +0100

    Added some code as a title to the quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 const App = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">App</div>
-  )
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-xl italic">
+        Ce quiz est conçu pour fournir des recommandations de bijoux
+        personnalisées en fonction de vos réponses.{' '}
+      </h1>
+      <h3 className=""> - Eloïne DA SILVA </h3>
+    </div>
+  );
 }
 export default App
